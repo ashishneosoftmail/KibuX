d6fa87827a6f0ba8c354d2bf338f4e8acc7d28de
Niranjan140 <niranjan@informationcapital.co.za>
2021-10-22 20:31:03 +0530

    Added kibu website

---
+++ Kibu/Kibu/Services/WebUrl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kibu.Services
+{
+    public static class WebUrl
+    {
+        public static string baseUrl = "http://154.0.166.250/Kibu";
+    }
+}
---
+++ Kibu/Kibu/View/KibuWebView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Acr.UserDialogs;
+using Kibu.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kibu.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class KibuWebView : ContentPage
+    {
+        public KibuWebView()
+        {
+            InitializeComponent();
+            if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                kibuWeb.Source = WebUrl.baseUrl;
+                kibuWeb.Navigated += KibuWeb_Navigated;
+                kibuWeb.Navigating += KibuWeb_Navigating;
+            }
+            else
+                DisplayAlert("Kibu", "Check your internet connection", "OK");
+        }
+
+        private void KibuWeb_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            UserDialogs.Instance.ShowLoading();
+        }
+
+        private void KibuWeb_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            UserDialogs.Instance.HideLoading();
+        }
+    }
+}
